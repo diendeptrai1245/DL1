9246a2584a5852a875601c1634194e8cfd6ba2ff
BuiDucDien <cudeptraik56@gmail.com>
2025-11-03 13:18:33 +0700

    commit 2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DL1
+You shuold use python version 3.8 -> 3.11 because tensorflow is support from version 3.8 to 3.11
+Download DATASET from https://www.kaggle.com/datasets/techsash/waste-classification-data?resource=download
+
+To running this project, following step by step:
+1.  python -m venv venv 
+2.  run file .\venv\Scripts\Activate.ps1 in powershell
+3.  run pip install -r requirements.txt to install all the library need for run code
---
+++ train.py
@@ -0,0 +1,24 @@
+from src.data_preprocessing import load_data
+from src.model import build_model
+from src.augmentation import get_augmented_data
+
+train_dir = "DATASET/TRAIN"
+test_dir = "DATASET/TEST"
+
+# Load data và augment
+train_data = get_augmented_data(train_dir)
+_, test_data = load_data(train_dir, test_dir)
+
+# Tạo model
+model = build_model()
+
+# Huấn luyện
+history = model.fit(
+    train_data,
+    validation_data=test_data,
+    epochs=10
+)
+
+# Lưu model
+model.save("cnn_rubbish_classifier.h5")
+print("✅ Model saved successfully!")
